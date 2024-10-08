be7b3ec0897a38be222c4bc8693d7536ae9f05dd
Derrick Gennrich <dgennrich@gmail.com>
2024-10-08 13:42:16 -0500

    Updated base character class directory location

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+// Copyright Derrick Gennrich
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}

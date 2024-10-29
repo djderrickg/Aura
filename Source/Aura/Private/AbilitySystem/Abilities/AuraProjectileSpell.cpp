45f7c4aea0df7d51f03361c90c0e39c4ca3221e6
Derrick Gennrich <dgennrich@gmail.com>
2024-10-29 13:24:18 -0500

    Added firebolt projectile spawning on LMB click

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,45 @@
+// Copyright Derrick Gennrich
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+#include "Actor/AuraProjectile.h"
+#include "Interaction/CombatInterface.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, 
+	const FGameplayAbilityActorInfo* ActorInfo, 
+	const FGameplayAbilityActivationInfo ActivationInfo, 
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	// Determine if on server or not
+	const bool bIsServer = HasAuthority(&ActivationInfo);
+
+	if (!bIsServer)
+	{
+		return;
+	}
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
+	if (CombatInterface)
+	{
+		const FVector SocketLocation = CombatInterface->GetCombatSocketLocation();
+		
+		// Spawn location for projectile is the socket location on Aura's weapon
+		FTransform SpawnTransform;
+		SpawnTransform.SetLocation(SocketLocation);
+		// TODO: Set projectile rotation
+
+		AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(ProjectileClass,
+			SpawnTransform,
+			GetOwningActorFromActorInfo(),
+			Cast<APawn>(GetOwningActorFromActorInfo()),
+			ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+
+		// TODO: Give the projectile a gameplay effect spec for causing damage
+
+		Projectile->FinishSpawning(SpawnTransform);
+	}
+
+
+}
